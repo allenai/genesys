0d12a5f081ca220e2dc82d28355bfcd948f92793
Junyan Cheng <chengjunyan1@gmail.com>
2024-11-08 20:12:57 +0000

    1

--- model_discovery/agents/prompts/prompts.py
+++ model_discovery/agents/prompts/prompts.py
@@ -208,7 +208,6 @@ Please try to fix the code based on the information provided. Do not include any
 def build_GU_QUERY(seeds,refs=None,instruct=None,user_input=None,
          mode=DesignModes.MUTATION,mutation_no_tree=True,scratch_no_tree=False):
    if mode==DesignModes.MUTATION:
-      seeds_prompt = seeds[0].to_prompt() if seeds else ''
       query = f"""
 # Seed Design
 
@@ -216,14 +215,14 @@ You are tasked with improving the following seed design:
 
 ---
 
-{seeds_prompt}
+{seeds[0].to_prompt() if seeds else ''}
 
 ---
 """
    elif mode==DesignModes.CROSSOVER:
       seeds_prompt='\n\n'
       for idx,seed in enumerate(seeds):
-         seeds_prompt+=f'---\n\n<details><summary>Parent {idx+1}</summary>{seed.to_prompt()}</details>\n\n'
+         seeds_prompt+=f'---\n\n<details><summary>Parent {idx+1}</summary>{seed.to_prompt() if seed else ''}</details>\n\n'
       query = f"""
 # Seed Design
 
@@ -262,11 +261,11 @@ Here are the relevant references:
             if reference is None:
                continue
             if reference.type in ['DesignArtifactImplemented','ReferenceCoreWithTree']:
-               ref_prompt = reference.to_prompt(full_tree=False)
+               ref_prompt = reference.to_prompt(full_tree=False) if reference else ''
             else:
-               ref_prompt = reference.to_prompt()
+               ref_prompt = reference.to_prompt() if reference else ''
          else:
-            ref_prompt = reference.to_prompt()
+            ref_prompt = reference.to_prompt() if reference else ''
          query += f"\nReference {idx} from library {reference.type}:\n{ref_prompt}\n\n---\n"
    if instruct:
       query += f"\nHere are some additional instructions that may help you:\n{instruct}\n\n---\n"
