a1801509bfbe4826800235f9a955866cda428395
Junyan Cheng <chengjunyan1@gmail.com>
2024-11-15 18:38:42 +0000

    1

--- bin/pages/evolve.py
+++ bin/pages/evolve.py
@@ -740,9 +740,9 @@ def bench_summary(evosys):
                 state,n_tries = node.state.split(':')
                 raw_states[node.acronym] = state
                 threshold = 5
-                if ('implemented' in state or 'succeeded' in state) and int(n_tries)<threshold:
+                if ('implemented' in state or 'succeeded' in state) and int(n_tries)<=threshold:
                     status[node.acronym] = 'succeeded'
-                elif int(n_tries)<threshold:
+                elif int(n_tries)<=threshold:
                     status[node.acronym] = 'unfinished'
                 else:
                     status[node.acronym] = 'failed'
