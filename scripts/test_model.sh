c3acc87c8a40c8ed5aba18975c2eb55c8d78df47
Junyan Cheng <chengjunyan1@gmail.com>
2024-12-25 10:18:26 +0000

    1

--- scripts/test_model.sh
+++ scripts/test_model.sh
@@ -1,2 +1,22 @@
-python -m model_discovery.model.library.tester --mode check "$@"
+
+
+
+# Parse command line arguments
+no_check=false
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -n|--no-check)
+            no_check=true
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# Run the check only if no_check is false
+if [ "$no_check" = false ]; then
+    python -m model_discovery.model.library.tester --mode check "$@"
+fi
 python -m model_discovery.model.library.tester --mode run "$@"
